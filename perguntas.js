3377f9b09b809b23173c2bf9999027b24db18a6f
Brayan Wesley Camargo Garcia <00001096777198sp@al.educacao.sp.gov.br>
2024-11-29 21:40:29 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'fisica',
+    'qual é a velocidade da luz?',
+    'a velocidade é de 299 792 458 m / s'
+)
+
+criaCartao(
+    'fisica',
+    'qual é a velocidade do som?',
+    '340,29 m / s'
+)
+
+criaCartao(
+    'fisica',
+    'como fazer a variação de algo?',
+    '(ValorAtual-ValorBase) = variação'
+)
+
+criaCartao(
+    'fisica',
+    'o que acontece se dois corpos de diferentes temperaturas se juntam?',
+    'A troca de calor acontece quando dois ou mais corpos com temperaturas diferentes são colocados em contato em um mesmo ambiente (sistema isolado) e, depois de certo tempo, alcançam o equilíbrio térmico. O sistema isolado referido acima é mais conhecido como calorímetro.'
+)
